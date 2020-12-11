1eb78f2f86e4b5e072c87592282b6db12799d48c
Risaki <kyozyu_masa@yahoo.co.jp>
2020-12-11 13:06:31 +0900

    スクリプトを作成するエディタ拡張のプログラムを追加

---
+++ 3d_project/Assets/Scripts/Editor/ScriptCreator/SceneNamesScriptCreator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// シーン名を管理するクラスのスクリプトを作成するクラス
+/// </summary>
+public static class SceneNamesScriptCreator
+{
+private const string MENU_ITEM_NAME     = "Tools/Create/Scene Name";
+private const string SCRIPT_FILE_PATH   = "Assets/Scripts/Constants/SceneNames.cs";
+
+    /// <summary>
+    /// シーンリストを変更した時の処理
+    /// </summary>
+    [MenuItem( MENU_ITEM_NAME )]
+    private static void Create() 
+    {
+        if ( !CanCreate() ) return;
+
+        CreateDirectoryIfNotExists();
+        var script_string = CreateScriptString();
+
+        File.WriteAllText( SCRIPT_FILE_PATH, script_string, Encoding.UTF8 );
+        AssetDatabase.Refresh( ImportAssetOptions.ImportRecursive );
+
+        EditorUtility.DisplayDialog( SCRIPT_FILE_PATH, "シーン名の作成が完了しました。", "OK" );
+    }
+
+    /// <summary>
+    /// 作成できるか判別する値を取得
+    /// </summary>
+    private static bool CanCreate() => !EditorApplication.isPlaying && !EditorApplication.isCompiling;
+
+    /// <summary>
+    /// スクリプトの文字列を作成
+    /// </summary>
+    private static string CreateScriptString() 
+    {
+        var contents = EditorBuildSettings.scenes
+            .Select( scene => Path.GetFileNameWithoutExtension( scene.path ) )
+            .Distinct()
+            .Aggregate( String.Empty, ( concated, scene_name ) => 
+            {
+                var adding_format_string    = @"    public const string {0} = ""{1}"";{2}";
+                var constant_name           = ToSnakeCaseFromUpperCamelCase( scene_name );
+
+                return concated + String.Format( 
+                    adding_format_string, 
+                    constant_name, 
+                    scene_name, 
+                    Environment.NewLine
+                );
+            } )
+        ;
+
+        var builder = new StringBuilder()
+            .AppendLine( "/// <summary>" )
+            .AppendLine( "/// シーンの名前を管理するクラス" )
+            .AppendLine( "/// <summary>" )
+            .AppendLine( "public static class SceneNames" )
+            .AppendLine( "{" )
+            .Append( contents )
+            .AppendLine( "}" )
+        ;
+
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// アッパーキャメルケースからスネークケースに変換
+    /// </summary>
+    private static string ToSnakeCaseFromUpperCamelCase( string value ) 
+    {
+        return Regex.Replace( value, "([a-z])([A-Z])", "$1_$2" ).ToUpper();
+    }
+
+    /// <summary>
+    /// ディレクトリが存在しない場合、作成
+    /// </summary>
+    private static void CreateDirectoryIfNotExists() 
+    {
+        var directory_path = Path.GetDirectoryName( SCRIPT_FILE_PATH );
+        if( Directory.Exists( directory_path ) ) return;
+        
+        Directory.CreateDirectory( directory_path );
+    }
+}
---
+++ 3d_project/Assets/Scripts/Editor/ScriptCreator/SceneTypeScriptCreator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEditor;
+
+/// <summary>
+/// シーンの種類を管理する列挙型のスクリプトを作成するクラス
+/// </summary>
+public static class SceneTypeScriptCreator
+{
+    private const string MENU_ITEM_NAME     = "Tools/Create/Scene Type";
+    private const string SCRIPT_FILE_PATH   = "Assets/Scripts/Constants/SceneType.cs";
+
+    /// <summary>
+    /// シーンリストを変更した時の処理
+    /// </summary>
+    // [MenuItem( MENU_ITEM_NAME )]
+    private static void Create() 
+    {
+        if ( !CanCreate() ) return;
+
+        CreateDirectoryIfNotExists();
+        var script_string = CreateScriptString();
+
+        File.WriteAllText( SCRIPT_FILE_PATH, script_string, Encoding.UTF8 );
+        AssetDatabase.Refresh( ImportAssetOptions.ImportRecursive );
+
+        EditorUtility.DisplayDialog( SCRIPT_FILE_PATH, "シーンの種類の作成が完了しました。", "OK" );
+    }
+
+    /// <summary>
+    /// 作成できるか判別する値を取得
+    /// </summary>
+    private static bool CanCreate() => !EditorApplication.isPlaying && !EditorApplication.isCompiling;
+
+    /// <summary>
+    /// スクリプトの文字列を作成
+    /// </summary>
+    private static string CreateScriptString() 
+    {
+        var contents = EditorBuildSettings.scenes
+            .Select( scene => Path.GetFileNameWithoutExtension( scene.path ) )
+            .Distinct()
+            .Select( scene_name => ToSnakeCaseFromUpperCamelCase( scene_name ) )
+            .Aggregate( String.Empty, ( concated, scene_name ) => concated + $"    {scene_name},{Environment.NewLine}" )
+        ;
+
+        var builder = new StringBuilder()
+            .AppendLine( "/// <summary>" )
+            .AppendLine( "/// シーンの種類を管理する列挙型" )
+            .AppendLine( "/// <summary>" )
+            .AppendLine( "public enum SceneType" )
+            .AppendLine( "{" )
+            .Append( contents )
+            .AppendLine( "}" )
+        ;
+
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// アッパーキャメルケースからスネークケースに変換
+    /// </summary>
+    private static string ToSnakeCaseFromUpperCamelCase( string value ) 
+    {
+        return Regex.Replace( value, "([a-z])([A-Z])", "$1_$2" ).ToUpper();
+    }
+
+    /// <summary>
+    /// ディレクトリが存在しない場合、作成
+    /// </summary>
+    private static void CreateDirectoryIfNotExists() 
+    {
+        var directory_path = Path.GetDirectoryName( SCRIPT_FILE_PATH );
+        if( Directory.Exists( directory_path ) ) return;
+        
+        Directory.CreateDirectory( directory_path );
+    }
+}
