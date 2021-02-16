7b2b177d7bf0b8db293a877aeb9535b5de38067c
Risaki <kyozyu_masa@yahoo.co.jp>
2021-02-16 16:00:24 +0900

    一旦コミット

---
+++ 3d_project/Assets/Scripts/MovementRepeater.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 移動の繰り返しを行うクラス
+/// </summary>
+public sealed class MovementRepeater : MonoBehaviour
+{
+    [SerializeField]
+    private float start_position    = 0.0f;
+
+    [SerializeField]
+    private float end_position      = 0.0f;
+    
+    [SerializeField]
+    private float m_speed           = 0.0f;
+
+    /// <summary>
+    /// 更新する時の処理
+    /// </summary>
+    private void Update()
+    {
+        transform.Translate( new Vector3( m_speed, 0.0f, 0.0f ) );
+
+        var position = transform.localPosition;
+        if ( position.x > end_position ) return;
+
+        position.x              = start_position;
+        transform.localPosition = position;
+    }
+}
---
+++ 3d_project/Assets/Scripts/ScrollableImage.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// スクロールできる画像を管理するクラス
+/// </summary>
+public class ScrollableImage : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_image = null;
+
+    [SerializeField, Range( 0.0f, 3.0f )]
+    private float m_speed = 0.0f;
+
+    /// <summary>
+    /// 規定値に戻す
+    /// </summary>
+    private void Reset() 
+    {
+        m_image = GetComponent<Image>();
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    private void Update()
+    {
+        var offset_x    = Mathf.Repeat( Time.time * m_speed, 1.0f );
+        var offset      = new Vector2( offset_x, 0.0f );
+        
+        // TODO: 間違い↓ ※ Sprite-Defaultのマテリアルには、オフセットが存在しないため、uvを直でいじる必要がある
+        m_image.material.SetTextureOffset( "_MainTex", offset );
+    }
+}
